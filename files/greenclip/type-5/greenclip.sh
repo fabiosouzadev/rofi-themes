357028a6789dfce1b5e51e23c51b838514197580
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-07-15 10:31:06 -0300

    feat: add my customization

--- files/greenclip/type-5/greenclip.sh
+++ files/greenclip/type-5/greenclip.sh
@@ -8,13 +8,12 @@
 ## Available Styles
 #
 ## style-1     style-2     style-3     style-4     style-5
-## style-6     style-7     style-8     style-9     style-10
-## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/launchers/type-2"
-theme='style-8'
+dir="$HOME/.config/rofi/launchers/type-5"
+theme='style-1'
 
 ## Run
 rofi \
-    -show drun \
+    -show clipboard \
+    -modi "clipboard:greenclip print" \
     -theme ${dir}/${theme}.rasi
